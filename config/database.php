1d312dd5d92b6c8cb1df40d54b38f4e314ac04a6
Maria Chrysanthou <124024824+mariachrysa@users.noreply.github.com>
2024-11-23 13:30:17 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,13 +2,13 @@
 class Database {
     private static $connection;
 
-    // Method to get a single instance of the database connection
+    // Get a single instance of the database connection
     public static function getConnection() {
         if (!self::$connection) {
             $servername = "127.0.0.1";
-            $username = "root"; // Your phpMyAdmin user
-            $password = ""; // Your phpMyAdmin password
-            $dbname = "HIMAROS_DB"; // Your database name
+            $username = "root"; 
+            $password = ""; 
+            $dbname = "HIMAROS_DB"; 
 
             // Create connection
             self::$connection = new mysqli($servername, $username, $password, $dbname);
